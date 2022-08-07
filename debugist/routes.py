3c64f82b4ea2bdc5fd9b9461f1d5114e55dfb82c
develosi <96108993+develosi@users.noreply.github.com>
2022-08-07 20:18:41 +0100

    Add route code to render projects file

--- debugist/routes.py
+++ debugist/routes.py
@@ -6,3 +6,8 @@ from debugist.models import Project, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")    
