475aefdf5e4051618e37604da0b0f842e1ae84d2
develosi <96108993+develosi@users.noreply.github.com>
2022-08-07 15:34:51 +0100

    Add initialization code for nav sidebar

---
+++ debugist/static/js/script.js
@@ -0,0 +1,5 @@
+// side nav initialization function
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+  });
