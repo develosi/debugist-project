27e2b56a53826b34d46af4ba9a0061f370e6c23f
develosi <96108993+develosi@users.noreply.github.com>
2022-08-11 22:45:08 +0100

    Add in functions for datepicker and select

--- debugist/static/js/script.js
+++ debugist/static/js/script.js
@@ -1,5 +1,16 @@
-// side nav initialization function
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
+    // side nav initialization function 
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-  });
+
+    // date picker initialization function 
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization function 
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+});
