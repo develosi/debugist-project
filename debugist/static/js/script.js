dc60f33449e99e8dce00727efb855a4b6f40b66e
develosi <96108993+develosi@users.noreply.github.com>
2022-08-29 20:10:31 +0100

    Fix addEventListener function in script.js

--- debugist/static/js/script.js
+++ debugist/static/js/script.js
@@ -3,10 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-    // modal trigger 
-    let elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems, options);
-
     // date picker initialization function 
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
@@ -22,3 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
 });
+
+
+// modal trigger 
+//let elems = document.querySelectorAll('.modal');
+//M.Modal.init(elems, options);
