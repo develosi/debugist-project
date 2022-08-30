bfc8941d22feb027745f553b4f99c43aa2bcc24a
develosi <96108993+develosi@users.noreply.github.com>
2022-08-30 19:37:55 +0100

    Add users schema to models.py

--- debugist/models.py
+++ debugist/models.py
@@ -26,3 +26,14 @@ class Task(db.Model):
         return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
+
+
+class Users(db.Model):
+    # schema for the Users model
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(260), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.user_name
